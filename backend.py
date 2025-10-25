c6dcc34df0367b7d20350169150247a8a65727e5
jdelpego <jdelpego@gmail.com>
2025-10-25 11:28:28 -0700

    Update optimize_meal response format to include foods as list with name and grams

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import numpy as np
 from scipy.optimize import lsq_linear
 from food_data import FOOD_DATA
+from typing import List
 
 app = FastAPI()
 
@@ -20,36 +21,22 @@ class MealRequest(BaseModel):
     target_carbs_percent: float
     target_protein_percent: float
     target_fat_percent: float
-    protein_food: str
-    carb_food: str
-    veggie_food: str
+    foods: List[str]
 
 @app.post("/optimize_meal")
 def optimize_meal(request: MealRequest):
+    foods = request.foods
     # Prepare per-gram vectors
-    protein_data = np.array([
-        FOOD_DATA[request.protein_food]["kcal"],
-        FOOD_DATA[request.protein_food]["carbs_g"],
-        FOOD_DATA[request.protein_food]["protein_g"],
-        FOOD_DATA[request.protein_food]["fat_g"]
-    ])
-    carb_data = np.array([
-        FOOD_DATA[request.carb_food]["kcal"],
-        FOOD_DATA[request.carb_food]["carbs_g"],
-        FOOD_DATA[request.carb_food]["protein_g"],
-        FOOD_DATA[request.carb_food]["fat_g"]
-    ])
-    veggie_data = np.array([
-        FOOD_DATA[request.veggie_food]["kcal"],
-        FOOD_DATA[request.veggie_food]["carbs_g"],
-        FOOD_DATA[request.veggie_food]["protein_g"],
-        FOOD_DATA[request.veggie_food]["fat_g"]
-    ])
-    
-    protein_per_g = protein_data / 100.0
-    carb_per_g = carb_data / 100.0
-    veggie_per_g = veggie_data / 100.0
-    A = np.column_stack((protein_per_g, carb_per_g, veggie_per_g))
+    food_data_list = []
+    for food in foods:
+        food_data = np.array([
+            FOOD_DATA[food]["kcal"],
+            FOOD_DATA[food]["carbs_g"],
+            FOOD_DATA[food]["protein_g"],
+            FOOD_DATA[food]["fat_g"]
+        ])
+        food_data_list.append(food_data / 100.0)
+    A = np.column_stack(food_data_list)
 
     # Compute targets
     carbs_g_target = (request.target_kcal * (request.target_carbs_percent / 100.0)) / 4.0
@@ -64,21 +51,12 @@ def optimize_meal(request: MealRequest):
     WA = row_weights[:, None] * A_s
     Wb = row_weights * b_s
 
-    lower_bounds = np.array([
-        FOOD_DATA[request.protein_food]["min_g"],
-        FOOD_DATA[request.carb_food]["min_g"],
-        FOOD_DATA[request.veggie_food]["min_g"]
-    ])
-    upper_bounds = np.array([
-        FOOD_DATA[request.protein_food]["max_g"],
-        FOOD_DATA[request.carb_food]["max_g"],
-        FOOD_DATA[request.veggie_food]["max_g"]
-    ])
+    lower_bounds = np.array([FOOD_DATA[food]["min_g"] for food in foods])
+    upper_bounds = np.array([FOOD_DATA[food]["max_g"] for food in foods])
 
     res = lsq_linear(WA, Wb, bounds=(lower_bounds, upper_bounds), lsmr_tol='auto', verbose=0)
     amounts_g = res.x
 
-    protein_g, carb_g, veggie_g = amounts_g
     achieved = A.dot(amounts_g)
 
     carbs_percent = (achieved[1] * 4.0) / achieved[0] * 100.0
@@ -86,9 +64,7 @@ def optimize_meal(request: MealRequest):
     fat_percent = (achieved[3] * 9.0) / achieved[0] * 100.0
 
     result = {
-        f"{request.protein_food}_g": round(protein_g, 1),
-        f"{request.carb_food}_g": round(carb_g, 1),
-        f"{request.veggie_food}_g": round(veggie_g, 1),
+        "foods": [{"name": food, "grams": round(amount, 1)} for food, amount in zip(foods, amounts_g)],
         "achieved_kcal": round(achieved[0], 1),
         "achieved_carbs_percent": round(carbs_percent, 1),
         "achieved_protein_percent": round(protein_percent, 1),
@@ -96,14 +72,12 @@ def optimize_meal(request: MealRequest):
     }
 
     # Add micronutrients
-    for key in FOOD_DATA[request.protein_food].keys():
-        if key not in ["kcal", "carbs_g", "protein_g", "fat_g", "fiber_g", "sugar_g"]:
-            result[key] = round(
-                protein_g * FOOD_DATA[request.protein_food][key] / 100 +
-                carb_g * FOOD_DATA[request.carb_food][key] / 100 +
-                veggie_g * FOOD_DATA[request.veggie_food][key] / 100,
-                1
-            )
+    micronutrients = {}
+    for key in FOOD_DATA[foods[0]].keys():
+        if key not in ["kcal", "carbs_g", "protein_g", "fat_g", "fiber_g", "sugar_g", "min_g", "max_g"]:
+            total = sum(amounts_g[i] * FOOD_DATA[foods[i]][key] / 100 for i in range(len(foods)))
+            micronutrients[key] = round(total, 1)
+    result.update(micronutrients)
 
     return result
 
